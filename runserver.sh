59c0052eaf5838789affdeb61b626ded1f4d24bd
Tobi DEGNON <tobidegnon@proton.me>
2023-09-18 16:44:03 +0100

    chore: fastwsgi from dependencies

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
+# fastwsgi config.wsgi:application --host 0.0.0.0 --port 8000
