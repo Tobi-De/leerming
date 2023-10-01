75608b0dfc9e370c3e19ed8ff188ef07315aaaa2
Tobi DEGNON <tobidegnon@proton.me>
2023-10-01 14:25:38 +0100

    fix: unique email address signup form check

--- leerming/users/forms.py
+++ leerming/users/forms.py
@@ -1,6 +1,9 @@
 from allauth.account.forms import SignupForm as AllauthSignupForm
 from django import forms
 from django.http import HttpRequest
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
 
 
 class SignupForm(AllauthSignupForm):
@@ -10,17 +13,23 @@ class SignupForm(AllauthSignupForm):
     """
 
     full_name = forms.CharField(
-        label="Nom complet",
+        label=_("Nom complet"),
         widget=forms.TextInput(attrs={"placeholder": "Entrez votre nom complet"}),
         required=False,
     )
     short_name = forms.CharField(
-        label="Nom court",
+        label=_("Nom court"),
         widget=forms.TextInput(
             attrs={"placeholder": "Entrez votre petit nom ou un prénom"}
         ),
         required=False,
     )
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            self.add_error("email", _("Un compte existe déjà avec cette adresse email"))
+        return email
 
     def save(self, request: HttpRequest):
         user = super().save(request)
