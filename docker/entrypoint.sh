9ffd6e218de12defb23d526983f143248d18f047
Tobi DEGNON <tobidegnon@proton.me>
2023-10-03 09:10:42 +0100

    build: trying granian

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,5 @@ python manage.py compress
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py makesuperuser
-gunicorn config.wsgi --config="docker/gunicorn.conf.py"
+#gunicorn config.wsgi --config="docker/gunicorn.conf.py"
+granian --interface asgi config.asgi:application --host 0.0.0.0 --port 80 --workers 4
