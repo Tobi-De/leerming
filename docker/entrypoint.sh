3700a739f566939c5690fac4df10b486e80f5ec2
Tobi DEGNON <tobidegnon@proton.me>
2023-10-03 08:19:09 +0100

    fix: invalid file reference because of anti slash

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#python manage.py compress
-#python manage.py collectstatic --noinput
+python manage.py compress
+python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py makesuperuser
 gunicorn config.wsgi --config="docker/gunicorn.conf.py"
