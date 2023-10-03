7783be0140b7ad559dc9aa336baada4aa9387778
Tobi DEGNON <tobidegnon@proton.me>
2023-10-03 18:49:50 +0100

    build: deploy qcluster with fly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py makesuperuser
 #gunicorn config.wsgi --config="docker/gunicorn.conf.py"
-granian --interface wsgi config.wsgi:application --host 0.0.0.0 --port 80 --workers 4
+#granian --interface wsgi config.wsgi:application --host 0.0.0.0 --port 80 --workers 4
