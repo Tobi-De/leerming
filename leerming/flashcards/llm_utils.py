441cf6a97cf88fcad457867724bc14ffcf36311e
Tobi DEGNON <tobidegnon@proton.me>
2023-10-15 20:51:08 +0100

    wip: add checkbox at triage step

--- leerming/flashcards/llm_utils.py
+++ leerming/flashcards/llm_utils.py
@@ -4,17 +4,15 @@ I can easily adapt to my needs.
 The original prompt https://thevitalcurriculum.notion.site/AI-Master-Guides-ca4db7147f3e43f5a92bb297142280f8
 """
 
-from langchain.output_parsers import PydanticOutputParser
-from pydantic import BaseModel, model_validator
-from dataclasses import dataclass, asdict
-
-from langchain.schema import BaseOutputParser
-from django.http import HttpRequest
 import re
-from langchain.prompts import PromptTemplate
-from langchain.llms import OpenAI
+import uuid
+from dataclasses import dataclass, asdict
 from functools import cached_property
 
+from django.http import HttpRequest
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import BaseOutputParser
 
 CLOZED_DELETION_TEMPLATE = """
 REVIEW_TEXT: "{source_text}"
@@ -43,19 +41,21 @@ Example:
 Sql stands or {{c1::Structured Query Language}} and is used to communicate with databases.|Sql is an {{c1::ANSI}} standard language that is used by all {{c2::relational}} database management systems (RDMS).
 """
 
+
 @dataclass
 class UnsavedFillInTheGapCard:
-    question:str
+    id: str
+    question: str
     answer: str
 
     @cached_property
     def split_on_answer(self):
         return self.question.split(self.answer)
-    
+
     @property
     def before_answer(self):
         return self.split_on_answer[0]
-    
+
     @property
     def after_answer(self):
         return self.split_on_answer[1]
@@ -64,11 +64,9 @@ class UnsavedFillInTheGapCard:
 class PipeSeparatedListOutputParser(BaseOutputParser):
     """Parse the output of an LLM call to a comma-separated list."""
 
-
     def parse(self, text: str):
         """Parse the output of an LLM call."""
         return text.strip().split("|")
-
 
 
 llm = OpenAI()
@@ -76,48 +74,49 @@ prompt = PromptTemplate.from_template(CLOZED_DELETION_TEMPLATE)
 chain = prompt | llm | PipeSeparatedListOutputParser()
 
 
-
-
 def extract_clozed_deletions(statement):
-    cloze_pattern = re.compile(r'{c\d+::(.*?)}')
+    cloze_pattern = re.compile(r"{c\d+::(.*?)}")
     cloze_matches = re.findall(cloze_pattern, statement)
-    cleaned_statement = re.sub(r'{c\d+::(.*?)}', r'\1', statement)
-    
-    return {
-        "statement": cleaned_statement.strip(),
-        "clozed_deletions": cloze_matches
-    }
+    cleaned_statement = re.sub(r"{c\d+::(.*?)}", r"\1", statement)
+
+    return {"statement": cleaned_statement.strip(), "clozed_deletions": cloze_matches}
 
 
-
-def make_flashcards_from(source_text:str, main_focus_point:str, max_result:int=10):
-    result =  chain.invoke(
+def make_flashcards_from(source_text: str, main_focus_point: str, max_result: int = 10):
+    result = chain.invoke(
         {
             "source_text": source_text,
             "main_focus_point": main_focus_point,
             "min_result": max_result,
-        })
+        }
+    )
     flashcards = []
     for statement in result:
         statement, clozed_deletions = extract_clozed_deletions(statement).values()
-        for cz in clozed_deletions:
-            flashcards.append(UnsavedFillInTheGapCard(statement, cz))
+        flashcards.extend(
+            UnsavedFillInTheGapCard(id=str(uuid.uuid4()), question=statement, answer=cz)
+            for cz in clozed_deletions
+        )
     return flashcards
 
 
-
-
-def save_tmp_flashcard_to_session(request:HttpRequest, flashcards:list[UnsavedFillInTheGapCard]):
+def save_tmp_flashcard_to_session(
+    request: HttpRequest, flashcards: list[UnsavedFillInTheGapCard]
+):
     request.session["tmp_flashcards"] = [asdict(flashcard) for flashcard in flashcards]
 
 
-def load_tmp_flashcard_from_session(request:HttpRequest):
+def load_tmp_flashcard_from_session(request: HttpRequest):
     try:
-        return [UnsavedFillInTheGapCard(**flashcard) for flashcard in request.session["tmp_flashcards"]]
+        return [
+            UnsavedFillInTheGapCard(**flashcard)
+            for flashcard in request.session["tmp_flashcards"]
+        ]
     except KeyError:
         return []
-    
-def delete_tmp_flashcard_from_session(request:HttpRequest):
+
+
+def delete_tmp_flashcard_from_session(request: HttpRequest):
     try:
         del request.session["tmp_flashcards"]
     except KeyError:
